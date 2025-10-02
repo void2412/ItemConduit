6ad91af16089efff51ba3f80396d9b38c274d301
void2412 <void2412@gmail.com>
2025-10-03 04:35:15 +0930

    Added interface to handle multiple types of container.

---
+++ ItemConduit/Interfaces/IContainerInterface.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ItemConduit.Interfaces
+{
+	
+    /// <summary>
+    /// Interface for interacting with different container types in Valheim
+    /// </summary>
+    public interface IContainerInterface
+	{
+		bool CanAddItem(ItemDrop.ItemData item);
+		bool CanRemoveItem(ItemDrop.ItemData item);
+		bool AddItem(ItemDrop.ItemData item);
+		bool RemoveItem(ItemDrop.ItemData item, int amount = 1);
+		List<ItemDrop.ItemData> GetAllItems();
+		int GetEmptySlots();
+		string GetName();
+		bool IsValid();
+	}
+}
+
